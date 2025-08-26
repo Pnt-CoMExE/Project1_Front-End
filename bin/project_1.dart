03f5f47e82cd87d74ad265109e1449ad0bd09731
Caglet <frewgametv@gmail.com>
2025-08-27 00:44:54 +0700

    finish delete front 6631501073

--- bin/project_1.dart
+++ bin/project_1.dart
@@ -73,6 +73,37 @@ Future<void> menu(int userId, String username) async {
         print("------------- Delete an expense -------------");
           // Add code here
           //test
+          Future<void> deleteExpense(int userId) async {
+          print("Enter expense ID to delete:"); //ฟังก์ชั่นที่เขียนจะทำงานแบบ async และ ไม่คืนค่า
+          String? input = stdin.readLineSync()?.trim(); // trim คือ ตัดช่องว่างออกถ้ามันมีช่องว่าง
+
+          if (input == null || input.isEmpty) {
+            print("Invalid input"); //ดูว่า input เป็น ว่างเปล่า ถ้าใช่จะ error
+            return;
+          }
+
+          int? expenseId = int.tryParse(input); //แปลง String เป็น int ครับ และ int? คือ อนุญาตให้เป็นค่าว่างครับ
+          if (expenseId == null) {
+            print("Please enter a valid number");
+            return;
+          }
+
+          final url = Uri.parse('http://localhost:3000/expenses/$expenseId');
+          try {
+            final response = await http.delete(url); //ส่ง HTTP DELETE request ไปยัง URL ที่กำหนดครับ
+
+            if (response.statusCode == 200) {
+              final result = json.decode(response.body); //แปลง Json เป็น Dart object ครับ
+              print("👍 ${result['message']}");
+            } else {
+              print("🖕 Failed to delete expense: ${response.body}");
+            }
+          } catch (err) {
+            print("🖕 Error connecting to server: $err");
+          }
+        }
+
+        await deleteExpense(userId);
         break;
       case '6':
         print("------ Bye ------");
